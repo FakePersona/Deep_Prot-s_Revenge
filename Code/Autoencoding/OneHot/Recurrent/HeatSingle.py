d338273a15eb2615fbeab673da528a9915f4dec8
FakePersona <remy.sun@ens-rennes.fr>
2016-07-06 23:06:03 +0200

    plop

--- Code/Autoencoding/OneHot/Recurrent/HeatSingle.py
+++ Code/Autoencoding/OneHot/Recurrent/HeatSingle.py
@@ -10,8 +10,15 @@ f = open("Single.txt", 'r')
 stored = json.load(f)
 f.close()
 
+labels = ["Norme"]
+
+for i in range(20):
+    labels.append("x" + str(i))
+
+labels = labels + ["Hydropathie", "Charge", "Aliphatic", "Aromatic", "Neutre", "Acide", "Basique", "All-alpha", "alpha - beta"]
+
 df = pd.DataFrame(np.array(stored))
 
-data = [go.Heatmap( z=df.corr().values.tolist())]
+data = [go.Heatmap( z=df.corr().values.tolist(), x = labels, y = labels)]
 
-py.plot(data, filename='SingleOneRecHeatb')
+py.plot(data, filename='SingleOneRecHeatf')
